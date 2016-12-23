74a177599ed598aae746feb658e897c3662b1d70
SOLTANE Makrem <msoltane@EASYLIS>
2016-12-23 17:35:57 +0100

    update 3

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,8 @@
+class NavBarComponentController  {
+
+}
+
 export const navbar: angular.IComponentOptions = {
   templateUrl: 'src/app/navbar/navbar.template.html',
-};
+   controller: NavBarComponentController,
+   };
